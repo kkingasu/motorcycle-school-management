93c6638b9a2500a3e6208f00bb479d0311cfe9d2
jrjones9440 <jrjones9440@gmail.com>
2023-02-23 19:56:01 -0800

    Created basic UI for the menus of the Garage, will possibly need more added for formatting the result sets of DB queries

--- src/main/ui/garage/GARAGE.java
+++ src/main/ui/garage/GARAGE.java
@@ -1,4 +1,49 @@
 package main.ui.garage;
 
 public class GARAGE {
+    public final static String MAIN_MENU = "====================================================================\n"
+            + "Garage Management\n"
+            + "====================================================================\n"
+            + "1. Manage Bikes\n"
+            + "2. Manage Bike Maintenance\n"
+            + "3. Return to Main Menu\n"
+            + "Input a number value to select an option from above: ";
+
+    public final static String MANGAGE_BIKES_MENU = "====================================================================\n"
+            + "Manage Bikes\n"
+            + "====================================================================\n"
+            + "1. View Available Bikes\n"
+            + "2. Assign Bike to Course\n"
+            + "3. Remove Bike from Course\n"
+            + "4. Add New Bike to Garage\n"
+            + "5. Remove Bike from Garage\n"
+            + "6. Return to Garage Management Menu\n"
+            + "Input a number value to select an option from above: ";
+
+    public final static String AVAILABLE_BIKES_MENU = "====================================================================\n"
+            + "View Available Bikes\n"
+            + "====================================================================\n"
+            + "1. All Available Bikes Report\n"
+            + "2. Search Available Street Bikes\n"
+            + "3. Search Available Dirt Bikes\n"
+            + "4. Return to Manage Bikes Menu\n"
+            + "Input a number value to select an option from above: ";
+
+    public final static String MANAGE_BIKE_MAINTENANCE_MENU = "====================================================================\n"
+            + "Manage Bike Maintenance\n"
+            + "====================================================================\n"
+            + "1. Submit Work Order\n"
+            + "2. Complete Work Order\n"
+            + "3. View Active Work Order Report\n"
+            + "4. View Closed Work Orders\n"
+            + "5. Return to Garage Management Menu\n"
+            + "Input a number value to select an option from above: ";
+
+    public final static String CLOSED_WORK_ORDER_MENU = "====================================================================\n"
+            + "View Closed Work Order Reports\n"
+            + "====================================================================\n"
+            + "1. Total Closed Work Order Report\n"
+            + "2. Individual Bike Work Order Report\n"
+            + "3. Return to Manage Bike Maintenance Menu\n"
+            + "Input a number value to select an option from above: ";
 }
